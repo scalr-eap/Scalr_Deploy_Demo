e9dc3899b1bd4f0489af9df8c2f638887a71c501
Ryan <ryan.fee625@gmail.com>
2020-05-06 13:14:29 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,11 +13,11 @@ variable "ssh_key_name" {
   type = string
 }
 
-variable "ssh_private_key" {
-  description = "The text of SSH Private key. This will be formatted by the Terraform template.<br>This will be used in the remote workspace to allow Terraform to connect to the servers and run scripts to configure Scalr. It only exists in the workspace for the duration of the run."
-  type = string
-  default = "FROM_FILE"
-}
+#variable "ssh_private_key" {
+#  description = "The text of SSH Private key. This will be formatted by the Terraform template.<br>This will be used in the remote workspace to allow Terraform to connect to the servers and run scripts to configure Scalr. It only exists in the workspace for the duration of the run."
+#  type = string
+#  default = "FROM_FILE"
+#}
 
 variable "vpc" {
   type = string
